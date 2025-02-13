f6834285722325306ad99f70c9e562bac49bd03a
Olha <berezovskaolh@gmail.com>
2025-02-13 14:58:46 +0200

    HomeWork19 - added two files: registrationtests.cy.js with tests and updated suppor/commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,20 @@
 Cypress.Commands.add("login", (username, password) => {
-    cy.visit("/login");
-    cy.get("#username").type(username);
-    cy.get("#password").type(password);
-    cy.get("button[type=submit]").click();
+    cy.visit('https://guest:welcome2qauto@qauto.forstudy.space')
+    cy.get('.btn-outline-white').click();
+    cy.get('#signinEmail').type(username);
+    cy.get('#signinPassword').type(password, {sensitive: true});
+    cy.get('button.btn-primary:nth-child(2)').click();
 });
+
+Cypress.Commands.overwrite('type', (originalFn, element, text, options) => {
+    if (options && options.sensitive) {
+      options.log = false;
+      Cypress.log({
+        $el: element,
+        name: 'type',
+        message: '*'.repeat(text.length),
+      });
+    }
+    return originalFn(element, text, options);
+  });
+  
